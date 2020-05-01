0eece4a60e2fb3fb021b9fd0fbcc7d3feec4f73a
shoq <shoq.dh@gmail.com>
2020-05-01 20:24:07 +0300

    Initial Commit

---
+++ first_prject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first_prject
+//
+//  Created by Eng.shoqi aldh on 5/1/20.
+//  Copyright © 2020 Eng.shoqi aldh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
