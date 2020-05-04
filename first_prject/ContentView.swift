8a771451f8dde3ca267dfe7012a2f60343e9754d
shoq <shoq.dh@gmail.com>
2020-05-04 21:28:07 +0300

    last upadate

--- first_prject/ContentView.swift
+++ first_prject/ContentView.swift
@@ -9,13 +9,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+ @State var theker = " الله اكبر"
+ @State var counter = "0"
     var body: some View {
-        Text("Hello, World!")
+    ZStack {
+        BG()
+        VStack {
+            
+        Text(theker)
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .bold()
+            .multilineTextAlignment(.center)
+            .padding()
+        
+        Text(counter)
+                  .foregroundColor(.white)
+                  .multilineTextAlignment(.center)
+                  .font(.largeTitle)
+                  .padding()
+   
+                
+            }
+        
+        
+    
+    }
+        
+             .onTapGesture(){
+                self.counter = String(Int(self.counter)! + 1) }
+                .onLongPressGesture {
+                    self.theker = athkar.randomElement()!
+                    self.counter = "0"
+        }
+    .edgesIgnoringSafeArea(.all)
     }
 }
+  
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct BG: View {
+    var body: some View {
+        ZStack {
+            Image ( "BACKGROUND")
+                .resizable()
+            
+            VStack{
+                Image ("Header")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Image ("Mosque")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
     }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+
 }
---
+++ first_prject/athkarFile.swift
@@ -0,0 +1,14 @@
+//
+//  athkarFile.swift
+//  first_prject
+//
+//  Created by Eng.shoqi aldh on 5/4/20.
+//  Copyright © 2020 Eng.shoqi aldh. All rights reserved.
+//
+
+import Foundation
+var athkar = [ "سبحان الله" ,
+                  "  الحمدلله" ,
+                  " الله اكبر",
+                  " استغفرلله" ]
+
